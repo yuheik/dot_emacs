5348e61d783b66d04c324d7b1110c4dc7cc2ae2b
yuhei.k <yuhei.k@gmail.com>
2020-05-26 10:58:58 +0900

    Update markdown-mode.

--- libs/05_markdown-mode.el
+++ libs/05_markdown-mode.el
@@ -1,6 +1,10 @@
 (use-package markdown-preview-mode
-  :mode "\\.md\\'"
+  :mode "\\.md\\(wn\\)?"
   :config (progn
             ;; (setq markdown-preview-stylesheets (list "github.css"))
             (setq markdown-command "/usr/local/bin/pandoc")
-            ))
+            (add-hook 'before-save-hook nil))
+  :bind (:map markdown-mode-map
+              ("M-C-f" . markdown-demote)
+              ("M-C-b" . markdown-promote))
+  )
